64fbc403a57ae57f5a3859e34bc8f3ce94896e84
ysk3a <53593899+ysk3a@users.noreply.github.com>
2023-05-10 20:51:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-primeng-fablike-expand
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sqduea)
---
+++ src/app/demo/inputgroup-button-demo.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'inputgroup-button-demo',
+    templateUrl: './inputgroup-button-demo.html',
+    styleUrls: ['./inputgroup-button-demo.scss']
+})
+export class InputgroupButtonDemo {
+    collapsedFab: boolean = false;
+}
